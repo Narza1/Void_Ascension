3761b63e10a3231acc254e814660d0a4d12623d0
Narza <104778948+Narza1@users.noreply.github.com>
2023-05-23 23:47:04 +0200

    23 f

--- Assets/Firebase/Scripts/DatabaseTest.cs
+++ Assets/Firebase/Scripts/DatabaseTest.cs
@@ -2,6 +2,7 @@
 using Firebase.Database;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,26 +13,100 @@ public class DatabaseTest : MonoBehaviour
     DatabaseReference dbReference;
     void Start()
     {
-       userID = SystemInfo.deviceUniqueIdentifier;
-       dbReference = FirebaseDatabase.DefaultInstance.RootReference;
+        userID = SystemInfo.deviceUniqueIdentifier;
+        dbReference = FirebaseDatabase.DefaultInstance.RootReference;
     }
 
-   public void CreateUser(string username)
+
+
+    public void SaveCharacterData(int characterType, int level, int floor, int money, string weaponGUID, string accessoryGUID, string consumableGUID)
     {
-        User newUser = new User(Name.text);
-        string json = JsonUtility.ToJson(newUser);
-        dbReference.Child("users").Child(userID).SetRawJsonValueAsync(json);
+        DropItem drop = new DropItem("test", 4);
+        CharacterData characterData = new CharacterData();
+        characterData.CharacterType = characterType;
+        characterData.Lv = level;
+        characterData.Floor = floor;
+        characterData.Money = money;
+        characterData.WeaponGUID = weaponGUID;
+        characterData.AccessoryGUID = accessoryGUID;
+        characterData.ConsumableGUID = consumableGUID;
+        characterData.Drop = drop;
+
+        string json = JsonUtility.ToJson(characterData);
+        dbReference.Child("characters").Child(userID).SetRawJsonValueAsync(json);
+        //    User newUser = new User(Name.text);
+        //string json = JsonUtility.ToJson(newUser);
+        //dbReference.Child("Death").Child(userID).SetRawJsonValueAsync(json);
     }
-    
+    //public void SaveCharacterData(int characterType, int level, int floor, int money, string weaponGUID, string accessoryGUID, string consumableGUID, DropItem drop)
+    //{
+    //    CharacterData characterData = new CharacterData();
+    //    characterData.CharacterType = characterType;
+    //    characterData.Lv = level;
+    //    characterData.Floor = floor;
+    //    characterData.Money = money;
+    //    characterData.WeaponGUID = weaponGUID;
+    //    characterData.AccessoryGUID = accessoryGUID;
+    //    characterData.ConsumableGUID = consumableGUID;
+    //    characterData.Drop = drop;
+
+    //    // Convertir el objeto DropItem a formato JSON
+    //    string dropJson = JsonUtility.ToJson(drop);
+
+    //    // Agregar el JSON del objeto DropItem a CharacterData
+    //    characterData.dropJson = dropJson;
+
+    //    string json = JsonUtility.ToJson(characterData);
+    //    dbReference.Child("characters").Child(userID).SetRawJsonValueAsync(json);
+    //}
+
+    public IEnumerator GetRandomCharacterByFloor(int targetFloor, Action<CharacterData> onCallBack)
+    {
+        var charactersRef = dbReference.Child("characters");
+
+        var query = charactersRef.OrderByChild("floor").EqualTo(targetFloor).GetValueAsync();
+        yield return new WaitUntil(() => query.IsCompleted);
+
+        if (query.Exception != null)
+        {
+            Debug.LogError($"Failed to retrieve character data: {query.Exception.Message}");
+            yield break;
+        }
+
+        if (query.Result != null && query.Result.Value != null)
+        {
+            List<DataSnapshot> matchingCharacters = new List<DataSnapshot>();
+
+            foreach (var childSnapshot in query.Result.Children)
+            {
+                matchingCharacters.Add(childSnapshot);
+            }
+
+            if (matchingCharacters.Count > 0)
+            {
+                int randomIndex = UnityEngine.Random.Range(0, matchingCharacters.Count);
+                string json = matchingCharacters[randomIndex].GetRawJsonValue();
+                CharacterData characterData = JsonUtility.FromJson<CharacterData>(json);
+                onCallBack.Invoke(characterData);
+                yield break;
+            }
+        }
+
+        Debug.Log("No character data found with the specified floor.");
+    }
+
+
+
     public IEnumerator GetName(Action<string> onCallBack)
     {
         var userNameData = dbReference.Child("users").Child(userID).Child("name").GetValueAsync();
         yield return new WaitUntil(predicate: () => userNameData.IsCompleted);
-        if(userNameData != null ) {
+        if (userNameData != null)
+        {
             DataSnapshot snapshot = userNameData.Result;
             onCallBack.Invoke(snapshot.Value.ToString());
         }
     }
 
-    
+
 }
--- Assets/Firebase/Scripts/User.cs
+++ Assets/Firebase/Scripts/User.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class User
 {
@@ -9,5 +11,54 @@ public class User
     {
         this.name = name;
     }
-    
+
+}
+
+[Serializable]
+public class CharacterData
+{
+    private int characterType, lv, floor, money;
+    private string weaponGUID, accessoryGUID, consumableGUID;
+    private DropItem drop;
+    public CharacterData()
+    {
+
+    }
+
+    public CharacterData(int characterType, int lv, int floor, int money, string weaponGUID, string accessoryGUID, string consumableGUID, DropItem drop)
+    {
+        this.characterType = characterType;
+        this.lv = lv;
+        this.floor = floor;
+        this.money = money;
+        this.weaponGUID = weaponGUID;
+        this.accessoryGUID = accessoryGUID;
+        this.consumableGUID = consumableGUID;
+        this.drop = drop;
+    }
+
+    public int CharacterType { get => characterType; set => characterType = value; }
+    public int Lv { get => lv; set => lv = value; }
+    public int Floor { get => floor; set => floor = value; }
+    public int Money { get => money; set => money = value; }
+    public string WeaponGUID { get => weaponGUID; set => weaponGUID = value; }
+    public string AccessoryGUID { get => accessoryGUID; set => accessoryGUID = value; }
+    public string ConsumableGUID { get => consumableGUID; set => consumableGUID = value; }
+    public DropItem Drop { get => drop; set => drop = value; }
+}
+
+[Serializable]
+public class DropItem
+{
+    private string itemGUID;
+    private int quantity;
+
+    public DropItem(string itemGUID, int quantity)
+    {
+        this.itemGUID = itemGUID;
+        this.quantity = quantity;
+    }
+
+    public string ItemGUI { get => itemGUID; set => itemGUID = value; }
+    public int Quantity { get => quantity; set => quantity = value; }
 }
